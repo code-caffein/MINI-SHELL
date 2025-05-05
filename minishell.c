03d04726e79e7c3591f9e7d204d3f441b1d1a92e
Abdelhamid El Haddadi <abel-had@e2r4p9.1337.ma>
2025-05-05 16:28:12 +0100

    DD

---
+++ minishell.c
@@ -0,0 +1,24 @@
+#include "../includes/minishell.h"
+
+int	main(int argc, char **argv, char **env)
+{
+	char input;
+	t_cmd *cmds;
+
+	while (1)
+	{
+		input = readline("minishell> ");
+		if (!input)
+		{
+			// printf("exit\n");
+			//generate a function that print exit\n and exit (ctrl + d)
+		}
+
+		if (input != '\0')
+			add_history(input);
+		cmds = parse(input);
+		free(input);
+    }
+
+    return 0;
+}
