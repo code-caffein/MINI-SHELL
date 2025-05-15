42ef0ee75d27b3ca4b9085c705c662c798a32670
Abdelhamid El Haddadi <abel-had@e3r8p5.1337.ma>
2025-05-15 11:17:11 +0100

    minishell.c

--- minishell.c
+++ minishell.c
@@ -2,50 +2,50 @@
 #include "./execution/execution.h"
 volatile sig_atomic_t g_signal_pid = 0;
 
-// void print_redirections(t_redirection *redir, const char *type) {
-//     while (redir != NULL) {
-//         printf("  %s: ", type);
-//         switch (redir->type) {
-//             case REDIR_IN:
-//                 printf("INPUT <%s (fd=%d)\n", redir->file, redir->fd);
-//                 break;
-//             case REDIR_OUT:
-//                 printf("OUTPUT >%s (fd=%d)\n", redir->file, redir->fd);
-//                 break;
-//             case REDIR_APPEND:
-//                 printf("APPEND >>%s (fd=%d)\n", redir->file, redir->fd);
-//                 break;
-//             case REDIR_HEREDOC:
-//                 printf("HEREDOC <<%s (fd=%d)\n", redir->file, redir->fd);
-//                 break;
-//             default:
-//                 printf("UNKNOWN REDIRECTION\n");
-//         }
-//         redir = redir->next;
-//     }
-// }
+void print_redirections(t_redirection *redir, const char *type) {
+    while (redir != NULL) {
+        printf("  %s: ", type);
+        switch (redir->type) {
+            case REDIR_IN:
+                printf("INPUT <%s (fd=%d)\n", redir->file, redir->fd);
+                break;
+            case REDIR_OUT:
+                printf("OUTPUT >%s (fd=%d)\n", redir->file, redir->fd);
+                break;
+            case REDIR_APPEND:
+                printf("APPEND >>%s (fd=%d)\n", redir->file, redir->fd);
+                break;
+            case REDIR_HEREDOC:
+                printf("HEREDOC <<%s (fd=%d)\n", redir->file, redir->fd);
+                break;
+            default:
+                printf("UNKNOWN REDIRECTION\n");
+        }
+        redir = redir->next;
+    }
+}
 
-// void print_commands(t_cmd *cmds) {
-//     int cmd_num = 1;
-//     while (cmds != NULL) {
-//         printf("Command #%d:\n", cmd_num++);
-//         // if (cmds->syn_err) {
-//         //     printf("  [Syntax Error]\n");
-//         //     cmds = cmds->next;
-//         //     continue;
-//         // }
-// 		printf("\nname :%s\n\n", cmds->name);
-//         // printf("  Name: %s\n", cmds->name ? cmds->name : "(null)");
-//         printf("  Arguments:");
-//         for (int i = 0; cmds->args && cmds->args[i]; i++) {
-//             printf(" [%s]", cmds->args[i]);
-//         }
-//         printf("\n");
-//         print_redirections(cmds->in, "Input Redirection");
-//         print_redirections(cmds->out, "Output Redirection");
-//         cmds = cmds->next;
-//     }
-// }
+void print_commands(t_cmd *cmds) {
+    int cmd_num = 1;
+    while (cmds != NULL) {
+        printf("Command #%d:\n", cmd_num++);
+        // if (cmds->syn_err) {
+        //     printf("  [Syntax Error]\n");
+        //     cmds = cmds->next;
+        //     continue;
+        // }
+		printf("\nname :%s\n\n", cmds->name);
+        // printf("  Name: %s\n", cmds->name ? cmds->name : "(null)");
+        printf("  Arguments:");
+        for (int i = 0; cmds->args && cmds->args[i]; i++) {
+            printf(" [%s]", cmds->args[i]);
+        }
+        printf("\n");
+        print_redirections(cmds->in, "Input Redirection");
+        print_redirections(cmds->out, "Output Redirection");
+        cmds = cmds->next;
+    }
+}
 
 void init_v(t_sp_var *v)
 {
@@ -74,35 +74,41 @@ int	main(int argc, char **argv, char **envp)
 	free_ptr(&(s.allocs), path);
 	while (1)
 	{
-		// if (g_signal_pid == -1)
-		// {
-		// 	status = 1;
-		// 	g_signal_pid = 0;
-		// }
+		if (g_signal_pid == -1)
+    	{
+        	v->status = 1;
+        	g_signal_pid = 0;
+    	}
 		if (!isatty(0) || !isatty(1))
 			return (1);
 		v->line = readline("minishell> ");
+		// printf("[%s]\n",v->line);
 		if (!v->line)
-        	v->line = ft_sttrdup("exit");
+        	v->line = ft_strdup("exit", &v->allocs, P_GARBAGE);
 		// printf("[%d]\n",status);
 		if (v->line[0] != '\0')
 			add_history(v->line);
 		if (g_signal_pid == -1)
-		{
-			v->status = 1;
-			g_signal_pid = 0;
-		}
+    	{
+        	v->status = 1;
+        	g_signal_pid = 0;
+        	// free(v->line);
+        	// continue;
+    	}
 		if (*v->line != '\0')
 		{
 			// printf("\n[%c]\n",*input);
+			// printf("dddddd\n");
 			v->cmds = parse(v);
 		// print_commands(cmds);
         // free_commands(cmds);
 		//check 
 			if (v->cmds )
 			{
-				// print_commands(cmds);
+				// print_commands(v->cmds);
+				// printf("hh\n");
         		// free_commands(cmds);
+				// printf("---------------------\n");
 				ft_execute(v->cmds, &v->status, &v->allocs, &v->env, envp);
 			}
 			else
